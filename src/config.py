f39830e9c19c71fd7e67c20806c6348162552766
5xgames <5xgames@example.com>
2026-02-24 15:53:47 +0800

    Security: Desensitize default URLs, project IDs and hostnames in config

--- src/config.py
+++ src/config.py
@@ -31,16 +31,16 @@ class Settings:
                 access_id=os.getenv('ALIYUN_AK_CN', ''),
                 access_key=os.getenv('ALIYUN_SK_CN', ''),
                 endpoint='http://service.odps.aliyun.com/api',
-                project='g13001230'
+                project=os.getenv('ALIYUN_PROJECT_CN', 'your_china_project')
             ),
             'holo': DBConfig(
                 access_id=os.getenv('ALIYUN_AK_CN', ''),
                 access_key=os.getenv('ALIYUN_SK_CN', ''),
-                host='hgprecn-cn-n6w1y5llh002-cn-hangzhou.hologres.aliyuncs.com',
-                port=80,
-                dbname='online',
-                user=os.getenv('ALIYUN_AK_CN', ''),
-                password=os.getenv('ALIYUN_SK_CN', '')
+                host=os.getenv('HOLO_HOST_CN', 'your_china_holo_host'),
+                port=int(os.getenv('HOLO_PORT_CN', '80')),
+                dbname=os.getenv('HOLO_DB_CN', 'online'),
+                user=os.getenv('ALI_USER_CN', os.getenv('ALIYUN_AK_CN', '')),
+                password=os.getenv('ALI_PASS_CN', os.getenv('ALIYUN_SK_CN', ''))
             )
         },
         'global': {
@@ -48,16 +48,16 @@ class Settings:
                 access_id=os.getenv('ALIYUN_AK_OVERSEAS', ''),
                 access_key=os.getenv('ALIYUN_SK_OVERSEAS', ''),
                 endpoint='http://service.ap-northeast-1.maxcompute.aliyun.com/api',
-                project='g65002007'
+                project=os.getenv('ALIYUN_PROJECT_GLOBAL', 'your_global_project')
             ),
             'holo': DBConfig(
                 access_id=os.getenv('ALIYUN_AK_OVERSEAS', ''),
                 access_key=os.getenv('ALIYUN_SK_OVERSEAS', ''),
-                host='hgpre-sg-6wr2ald3b002-ap-northeast-1.hologres.aliyuncs.com',
-                port=80,
-                dbname='online',
-                user=os.getenv('ALIYUN_AK_OVERSEAS', ''),
-                password=os.getenv('ALIYUN_SK_OVERSEAS', '')
+                host=os.getenv('HOLO_HOST_GLOBAL', 'your_global_holo_host'),
+                port=int(os.getenv('HOLO_PORT_GLOBAL', '80')),
+                dbname=os.getenv('HOLO_DB_GLOBAL', 'online'),
+                user=os.getenv('ALI_USER_GLOBAL', os.getenv('ALIYUN_AK_OVERSEAS', '')),
+                password=os.getenv('ALI_PASS_GLOBAL', os.getenv('ALIYUN_SK_OVERSEAS', ''))
             )
         }
     }
@@ -65,14 +65,14 @@ class Settings:
     # --- ThinkingData Credentials ---
     TA_CREDENTIALS = {
         'china': TAConfig(
-            url=os.getenv("TA_URL_CN", "https://ss-web.5xgames.cn/"),
-            sql_url=os.getenv("TA_SQL_URL_CN", "https://ss-web.5xgames.cn/#/tga/ide/-1"),
+            url=os.getenv("TA_URL_CN", "https://your-ta-china-url.com/"),
+            sql_url=os.getenv("TA_SQL_URL_CN", "https://your-ta-china-url.com/#/tga/ide/-1"),
             user=os.getenv("TA_USER_CN", ""),
             password=os.getenv("TA_PASS_CN", "")
         ),
         'global': TAConfig(
-            url=os.getenv("TA_URL_GLOBAL", "http://8.211.141.76:8993/"),
-            sql_url=os.getenv("TA_SQL_URL_GLOBAL", "http://8.211.141.76:8993/#/tga/ide/-1"),
+            url=os.getenv("TA_URL_GLOBAL", "https://your-ta-global-url.com/"),
+            sql_url=os.getenv("TA_SQL_URL_GLOBAL", "https://your-ta-global-url.com/#/tga/ide/-1"),
             user=os.getenv("TA_USER_GLOBAL", ""),
             password=os.getenv("TA_PASS_GLOBAL", "")
         )
